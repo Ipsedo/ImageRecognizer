eae51153063ae23a114bf9348e7ae0108090dc7b
samuel <berriens@gmail.com>
2018-05-06 19:04:08 +0200

    faire model

---
+++ scripts/model.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+class ConvModel(nn.Module):
+
+    def __init(self, imgSize, nbClass):
+        super(ConvModel, self).__init__()
+
+    def forward(self, input):
+        return input
